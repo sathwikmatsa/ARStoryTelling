8616ba70b3787f497cf4523ae3695788219d1533
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-02-05 23:24:09 +0530

    add parser.js based on PEG

--- ars.js
+++ ars.js
@@ -1,3 +1,4 @@
+let parser = PARSER;
 function loadFile(filePath) {
   var result = null;
   var xmlhttp = new XMLHttpRequest();
@@ -9,72 +10,6 @@ function loadFile(filePath) {
   return result;
 }
 
-let ars = loadFile('story.ars');
-
-while(ars.trim().length != 0){
-    let i,j;
-    let screenplay = {}, elems;
-    // extract next scene
-    i = ars.indexOf('<scene>');
-    j = ars.indexOf('</scene>');
-    let scene = ars.substring(i+7, j);
-    ars = ars.slice(j+8);
-
-    //process setup tag
-    i = scene.indexOf('<setup>');
-    j = scene.indexOf('</setup>');
-    let setup = scene.substring(i+7, j).trim();
-    elems = setup.split(";");
-    for(let k=0; k<elems.length-1; k++){
-        let elem = elems[k].trim();
-        let property = elem.split(":")[0].trim();
-        if(property === "background"){
-            screenplay["background"] = elem.split(":")[1].trim().slice(1,-1);
-        } else if(property === "objects"){
-            let objs = [];
-            i = elem.indexOf('{');
-            j = elem.indexOf('}');
-            let actors = elem.substring(i+1, j);
-            actors = actors.split(",");
-            for(let m=0; m<actors.length; m++){
-                let url_p = actors[m].split(":")[0].trim().slice(1,-1);
-                let id_p = actors[m].split(":")[1].trim();
-                objs.push({url: url_p, id: id_p});
-            }
-            screenplay["objects"] = objs;
-        }
-    }
-
-    //process onload
-    i = scene.indexOf('<onload>');
-    j = scene.indexOf('</onload>');
-    let onload = scene.substring(i+8, j).trim();
-    elems = onload.split(";");
-    for(let k=0; k<elems.length-1; k++){
-        let elem = elems[k];
-        elem = elem.split("->");
-        let id = elem[0].trim();
-        screenplay[id] = {};
-        let properties = elem[1].split(",");
-        for(let m=0; m<properties.length; m++){
-            let property = properties[m];
-            screenplay[id][property.split(":")[0].trim()] = property.split(":")[1].trim()
-        }
-    }
-
-    //process play
-    i = scene.indexOf('<play>');
-    j = scene.indexOf('</play>');
-    let play = scene.substring(i+6, j).trim();
-    elems = play.split(";");
-    let frames = [];
-    for(let k=0; k<elems.length-1; k++){
-        let elem = elems[k].trim();
-        frames.push(elem);
-    }
-    screenplay["play"] = frames;
-
-    console.log(screenplay);
-    alert("check console for full screenplay object\n"+JSON.stringify(screenplay, null, 4));
-}
-
+let story = parser.parse(loadFile('story.ars'));
+alert(JSON.stringify(story), null, 2);
+console.log(story)
---
+++ parser_grammar.pegjs
@@ -0,0 +1,29 @@
+PROGRAM = a:SCENE+ {return a;}
+
+SCENE = _ "<scene>" _ a:DECL* _ "<onload>" _ b:SETATTRS* _ "</onload>" _ "<play>" _ c:ACTION* _ "</play>" _ "</scene>" _ {
+    return {
+        declarations: a.reduce((x,c)=>Object.assign(x, c)),
+        attributes: b,
+        actions: c
+    };
+}
+
+DECL
+= _ "background" _ ":" _ l:literal _ ";" _  {return {background: l};}
+/ _ "objects" _ ":" _ "{" _ a:(b:literal _ ":" _ c:id _ ","? _ {let x = {}; x[c]=b; return x;})+ _ "}" _ ";" _ {return {objects: a}}
+
+SETATTRS = _ a:id _ "->" _ d:(b:attr _ ":" _ c:val _ ","? _ {let x = {}; x[b]=c; return x;})+ _ ";" _ {return {actor: a, attributes:d.reduce((x,c)=>Object.assign(x, c)) };}
+
+ACTION
+= _ "subtitle" _ ":" _ r:literal _ ";" _ {return {subtitle: r};}
+/ _ a:id _ "(" _ b:action _ d:("," _ c:(literal/val/(m:id "." n:attr {let x = {}; x[m]=n; return x;})) {return c;})* _ ")" _ ";" _ {return {action:b, actor: a, parameters: d };}
+
+_ = [ \n\t\r]* 
+
+literal 
+= '"' r:[^\"]+ '"' {return r.reduce((a,c)=> a+c);} 
+/ r:[0-9]+ {return parseInt(r.reduce((a,c)=> a+c))}
+id = r:[a-zA-Z0-9]+ {return r.reduce((a,c)=> a+c);}
+attr = r:("position" / "size") {return r;}
+val = r:("rightend"/ "leftend"/ "middle"/ "slow"/ "fast"/ "medium"/ "default"/ "large"/ "small") {return r;} 
+action = r:("move"/ "dialog") {return r;}
